2dc7b265ffb25dde18ef2edb24081d6cdca8f598
Elijah Bobzom <ebobzom@gmail.com>
2020-04-12 16:27:03 +0100

    add logs logic

--- app.js
+++ app.js
@@ -17,33 +17,40 @@ app.use(morgan(':method :url :status :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+morgan.token('time', function getTime(){
+  return new Date().getTime();
+})
+
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 // setup the logger
-app.use(morgan(':method :url :status :response-time ms', { stream: accessLogStream }));
+app.use(morgan(':time   :url    done in :response-time s', { stream: accessLogStream }));
 
-app.get('/', (req, res) => {
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'access.log'));
 });
 
-app.post('/xml', (req, res) => {
+app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const builder = new xml2js.Builder();
   const xml = builder.buildObject(estimator(req.body));
   res.set('Content-Type', 'application/xml');
   res.status(200).send(xml);
 });
 
-app.post('/json', (req, res) => {
+app.post('/api/v1/on-covid-19/json', (req, res) => {
   res.set('Content-Type', 'application/json');
   res.status(200).json(estimator(req.body));
 });
 
-app.post('/', (req, res) => {
+app.post('/api/v1/on-covid-19/', (req, res) => {
   res.set('Content-Type', 'application/json');
   res.status(200).json(estimator(req.body));
 });
 
+app.use('*', (err,req, res) => {
+  console.log(err);
+})
 
 app.listen(port, (err) => {
   /* eslint no-console: off */
